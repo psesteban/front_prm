a1f25543e4af7ad0b70c95a65a8805c0271ba427
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2024-07-21 14:29:41 -0400

    revisando navegation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const Navigation = () => {
   const navigate = useNavigate()
   const { getProfesional, setProfesional } = useContext(Context)
+  if (getProfesional) console.log('entraste como administrador')
   const logout = () => {
     setProfesional(null)
     window.sessionStorage.removeItem('token')
