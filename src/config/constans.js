63f5485d465c74f77582173f4b62a7c71b135848
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2024-09-05 22:25:35 -0400

    habilitando server

--- src/config/constans.js
+++ src/config/constans.js
@@ -1,4 +1,4 @@
-export const URLBASE = 'http://localhost:3000'
+export const URLBASE = 'https://server-as9p.onrender.com'
 // 'https://server-as9p.onrender.com' o 'http://localhost:3000'
 
 export const ENDPOINT = {
