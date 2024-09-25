0e0497186f026165c86823319a736bb7de87d937
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2024-09-25 17:25:26 -0300

    mejorando not found

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -4,10 +4,9 @@ import './NotFound.css'
 const NotFound = () => {
   return (
     <div className='not-found'>
-      <img src='../../public/assets/notfound.webp' alt='Página no encontrada' />
       <h2>Página no encontrada</h2>
       <p>Lo sentimos, la página que estás buscando no existe.</p>
-      <Link to='/'>Volver a la página principal</Link>
+      <button> <Link to='/'>Volver a la página principal</Link></button>
     </div>
   )
 }
