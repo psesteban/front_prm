2caedecc4606e620ab71c3940a5e33861eb9041f
psesteban <psicologoclinicoesteban@gmail.com>
2024-12-22 00:42:58 -0300

    configurando firebase

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,14 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD0HXWRy79KhFykCVDtB4ELZ5PWN0l_zuM',
-  authDomain: 'friendly-lamp-427416-r4.firebaseapp.com',
-  projectId: 'friendly-lamp-427416-r4',
-  storageBucket: 'friendly-lamp-427416-r4.appspot.com',
-  messagingSenderId: '213239840470',
-  appId: '1:213239840470:web:ff902e45e977fbf9541377'
+  apiKey: "AIzaSyAR18iQZKavcQdayDO6Uwx5FWAqa8jSNtw",
+  authDomain: "sample-firebase-psc.firebaseapp.com",
+  projectId: "sample-firebase-psc",
+  storageBucket: "sample-firebase-psc.firebasestorage.app",
+  messagingSenderId: "81413542602",
+  appId: "1:81413542602:web:9c3154489ec2713ddf1a33"
 }
-
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
